f99a50b9d406e079dd0bc56bcfa1d951df7a8272
bgmellado22 <bgmellado22@gmail.com>
2026-08-08 17:15:44 -0400

    Initial Commit

---
+++ SwiftFieldReporter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftFieldReporter
+//
+//  Created by Bastihan on 08-08-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftFieldReporter/SwiftFieldReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFieldReporterApp.swift
+//  SwiftFieldReporter
+//
+//  Created by Bastihan on 08-08-26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFieldReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
